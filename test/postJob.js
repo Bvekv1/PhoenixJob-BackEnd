e619e1e19b51a80c075451f1b25ad02278a0e8ce
bvekv1 <bvekp1@gmail.com>
2020-07-01 22:42:14 +0545

    mocha test passed

--- test/postJob.js
+++ test/postJob.js
@@ -6,20 +6,23 @@ chai.use(chaiHttp);
 
 var server = require('../index.js')
 
-describe('User',function(){
+describe('job',function(){
 
 	describe('POST Job in test',function(){
 
 		it('post job',function(done){
 
 		chai.request(server)
-			.post('/postJob')
-			.set('content-type','application/x-www-form-urlencoded')
+      .post('/postJob')
+      .set({"Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTkzNjE4Mzk5fQ.Rlsn2pHvL0NcfRyAK4XLHYr4-9EkawYPWxBi31jcGaA'})
+      .set('content-type','application/x-www-form-urlencoded')
+      
 			.send({
                 jobTitle:'Android developer',
                 experience:'2 years',
                 level:'bachelor',
                 positions:'head',
+                jobType:'full tume',
                 salary:'30000',
                 education:'bachelor',
                 Location:'ktm',
@@ -28,12 +31,14 @@ describe('User',function(){
                 jobQualification:'bachelor',
                 expected:'bachelor final running',
                 jobHours:'8',
-                benefits:'increasing salary with job progress',
-                userId:'1'
+                benefits:'increasing salary with job progress'
+            
+                
 			})
 			.end(function(err,res){
-
-				res.body.should.have.property('status').eql(200)
+              res.body.should.have.status('200')
+                //res.body.should.be.a('object');
+                //res.body.should.have.property('message').eql('sucess');
 				done();
 			})
 		})
