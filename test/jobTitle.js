4bdff28a51472de81c0579721c92d8481878f23a
Rohan Kharel <rohankharel86@gmail.com>
2020-07-04 13:52:51 +0545

    test 2

---
+++ test/jobTitle.js
@@ -0,0 +1,19 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+const request = require('supertest');
+
+chai.use(chaiHttp);
+
+var server = require('../index.js');
+const { assert } = require('chai');
+
+describe('GET/ user', function(){
+    it('responds with json', function(done){
+        request(server)
+        .get('/searchJobByTitle/:jobTitle')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+});
